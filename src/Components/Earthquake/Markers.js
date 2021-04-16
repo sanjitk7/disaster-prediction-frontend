928bad6e95d29d682a982904c78ec5392003ca62
Tanya Warrier <51485771+tanyarw@users.noreply.github.com>
2021-04-17 03:03:34 +0530

    colour markers

--- src/Components/Earthquake/Markers.js
+++ src/Components/Earthquake/Markers.js
@@ -7,16 +7,16 @@ import {
 const MyMarker=(props) =>{
     const circleMarkerColor = (magnitude)=> {
         return magnitude <= 1
-            ? '#00b800'
+            ? '#5b0a91'
             : magnitude > 1 && magnitude <= 2
-            ? '#b6fe00'
+            ? '#a8005b'
             : magnitude > 2 && magnitude <= 3
-            ? '#f6ff00'
+            ? '#ff6600'
             : magnitude > 3 && magnitude <= 5
-            ? '#ffcf00'
+            ? '#dc1c13'
             : magnitude > 5 && magnitude <= 7
-            ? '#ff9000'
-            : '#ff0000';
+            ? '#8b0000'
+            : '#000000';
     };
     let mydata = props.data
     let filteredData = props.fData
